2fbb848315408c5736103a55019340135f4664f7
Cuong Vo Duy <cuongvd@outlook.com>
2020-10-19 23:14:25 +0700

    Add message

---
+++ ERP.Ultilities/Global/RoleMessageGlobal.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace ERP.Ultilities.Global
+{
+    public class RoleMessageGlobal
+    {
+        public static IConfigurationSection configurationSection;
+        
+        static RoleMessageGlobal()
+        {
+            var builder = new ConfigurationBuilder().SetBasePath(Directory.GetCurrentDirectory()).AddJsonFile("appsettings.json", optional: true, reloadOnChange: true);
+            configurationSection = builder.Build().GetSection("Messages").GetSection("Role");
+        }
+
+        public static string GetMessage(string Key)
+        {
+            return configurationSection.GetSection(Key).Value;
+        }
+
+        //example get EmptyFirstName
+        //public static string EmptyFirstName => GetMessage("EmptyFirstName");
+    }
+}
