a42b63d37787e96254d4ea645cf6e1edf9b8f93a
Cuong Vo Duy <cuongvd@outlook.com>
2020-10-21 23:39:49 +0700

    Add common message

--- ERP.Ultilities/Global/CommonMessageGlobal.cs
+++ ERP.Ultilities/Global/CommonMessageGlobal.cs
@@ -25,5 +25,8 @@ namespace ERP.Ultilities.Global
         public static string _404 => GetMessage("404");
 
         public static string Require(string fieldName) => GetMessage("Require", fieldName);
+        public static string Minimum(string fieldName, int minCharacter) => GetMessage("Minimum", fieldName, minCharacter);
+        public static string Maximum(string fieldName, int maxCharacter) => GetMessage("Require", fieldName, maxCharacter);
+        public static string NotExistInCategory(string fieldName) => GetMessage("NotExistInCategory", fieldName);
     }
 }
