6226b5429433439d1b728168932ce78743a3c39d
1511164 hcmut.edu.vn <1511164@hcmut.edu.vn>
2020-11-01 13:01:48 +0700

    Add department statistic data

---
+++ ERP.Model/DataTransferObjects/Statistic/DepartmentStatisticDataTransfer.cs
@@ -0,0 +1,18 @@
+﻿using ERP.Model.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ERP.Model.DataTransferObjects
+{
+    /// <summary>
+    /// Báo cáo tình hình đi làm / nghỉ việc của từng department hàng ngày
+    /// </summary>
+    public class DepartmentStatisticDataTransfer
+    {
+        public string Name { get; set; }        //Tên department
+        public decimal? Percent { get; set; }   //Phần trăm nhân viên của department/Tổng số nhân viên
+        public int EmployeeCount { get; set; }  //Tổng số nhân viên của department
+        public int EmployeeLeave { get; set; }  //Số nhân viên chưa checkin của department
+    }
+}
