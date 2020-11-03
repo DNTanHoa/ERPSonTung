8fa8bb919213bb3ac8faad5e327f40a6ea9c0df1
Cuong Vo Duy <cuongvd@outlook.com>
2020-11-03 21:35:20 +0700

    add import validate

---
+++ ERP/Validators/ImportExcelValidator.cs
@@ -0,0 +1,25 @@
+﻿using ERP.Model.DataTransferObjects;
+using ERP.Model.Models;
+using ERP.Repository;
+using ERP.RequestModel.Employee;
+using ERP.Ultilities.Global;
+using FluentValidation;
+
+namespace ERP.Validators
+{
+    public class ImportEmployeeExcelFileValidator : AbstractValidator<EmployeeImportDataTransfer>
+    {
+        public ImportEmployeeExcelFileValidator()
+        {
+            RuleFor(x => x.Code).NotEmpty().WithMessage(CommonMessageGlobal.Require("Mã nhân viên"));
+            
+            RuleFor(x => x.LastName).NotEmpty().WithMessage(CommonMessageGlobal.Require("Tên nhân viên"))
+                .MinimumLength(2).WithMessage(CommonMessageGlobal.Minimum("Tên nhân viên", 2))
+                .MaximumLength(20).WithMessage(CommonMessageGlobal.Maximum("Tên nhân viên", 20));
+
+            RuleFor(x => x.FullName).NotEmpty().WithMessage(CommonMessageGlobal.Require("Họ và tên nhân viên"))
+                .MinimumLength(2).WithMessage(CommonMessageGlobal.Minimum("Họ và tên nhân viên", 2))
+                .MaximumLength(20).WithMessage(CommonMessageGlobal.Maximum("Họ và tên nhân viên", 256));
+        }
+    }
+}
